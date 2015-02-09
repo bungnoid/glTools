bc9a31a2f04877b61b9ed08233cb6897936b110a
Grant Laker <grant@bungnoid.com>
2015-02-09 10:45:34 -0800

    Added globals

---
+++ gl_global.py
@@ -0,0 +1,14 @@
+"""
+Globals Container Module
+"""
+
+# Data Object List
+dataList = []
+
+# Global SkinClusterData object
+# - Used by SkinClusterDataUI (glRig.ui.skinClusterData)
+glSkinClusterData = None
+
+# Global copy/paste skin weights command string
+# - Used by copy/paste skin weights (glRig.tools.copyPasteWeights)
+glCopyPasteWeightCmd = ''
